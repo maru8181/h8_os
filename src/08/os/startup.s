bf88024f510d6552b002451ede28886808766d65
maru8181 <stop_1_supasupa@yahoo.co.jp>
2023-07-05 11:06:03 +0900

    08 build

--- src/08/os/startup.s
+++ src/08/os/startup.s
@@ -9,3 +9,18 @@ _start:
 
 1:
     bra     1b              # 無限ループ
+
+	.global     _dispatch
+#	.type       _dispatch,@function
+
+_dispatch:
+	mov.l   @er0,er7
+	mov.l   @er7+,er0
+	mov.l   @er7+,er1
+	mov.l   @er7+,er2
+	mov.l   @er7+,er3
+	mov.l   @er7+,er4
+	mov.l   @er7+,er5
+	mov.l   @er7+,er6
+	rte
+
